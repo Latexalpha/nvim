872ec46250d0cb50c966f93a1cb4d1d14e1d5b35
Windows Desktop <heihiha@outlook.com>
2023-03-25 22:22:11 +0800

    remove version setting

--- lua/plugins/LuaSnip.lua
+++ lua/plugins/LuaSnip.lua
@@ -2,7 +2,6 @@ local snippath = vim.fn.stdpath("config") .. "/lua/snippets"
 
 return {
     "L3MON4D3/LuaSnip",
-	version = "<CurrentMajor>.*", -- follow latest release.
     config = function()
         require("luasnip.loaders.from_lua").lazy_load(
             { paths = snippath }
