7ed916779684b58d73facd6f6a3648ce0066062c
Windows Desktop <heihiha@outlook.com>
2023-03-27 15:02:26 +0800

    add shortcut for markdown preview

--- lua/plugins/markdown-preview.lua
+++ lua/plugins/markdown-preview.lua
@@ -1,6 +1,10 @@
 return {
     "iamcco/markdown-preview.nvim",
     event = "BufRead",
+    keys = {
+        {"<leader>mp", "<cmd>MarkdownPreview<CR>", desc = "MarkdownPreview"},
+        {"<leader>mt", "<cmd>MarkdownPreviewStop<CR>", desc = "MarkdownPreviewStop"},
+    },
     config = function()
         vim.fn["mkdp#util#install"]()
     end,
