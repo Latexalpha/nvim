7e69e0aa7392ade4d02af98a39476eacf5484f15
Windows Desktop <heihiha@outlook.com>
2023-04-04 13:05:45 +0800

    updation

--- lua/plugins/nvim-dap.lua
+++ lua/plugins/nvim-dap.lua
@@ -1,28 +1,43 @@
 return {
-    "mfussenegger/nvim-dap",
-    config = function()
-        local dap = require("dap")
-        dap.adapters.python = {
-            type = "executable",
-            command = vim.g.python3_host_prog,
-            args = { "-m", "debugpy.adapter" },
+    {
+        "rcarriga/nvim-dap-ui",
+        dependencies = {
+            "mfussenegger/nvim-dap",
         }
-        dap.configurations.python = {
-            {
-            -- The first three options are required by nvim-dap
-                type = 'python', -- the type here established the link to the adapter definition: `dap.adapters.python`
-                request = 'launch',
-                name = "Launch file",
+    },
+    {
+        "mfussenegger/nvim-dap",
+        config = function()
+            local dap = require("dap")
+            dap.adapters.python = {
+                type = "executable",
+                command = vim.g.python3_host_prog,
+                args = { "-m", "debugpy.adapter" },
+            }
+            dap.configurations.python = {
+                {
+                -- The first three options are required by nvim-dap
+                    type = 'python', -- the type here established the link to the adapter definition: `dap.adapters.python`
+                    request = 'launch',
+                    name = "Launch file",
 
-                -- Options below are for debugpy, see https://github.com/microsoft/debugpy/wiki/Debug-configuration-settings for supported options
-                program = "${file}", -- This configuration will launch the current file if used.
-                pythonPath = function()
-                    -- debugpy supports launching an application with a different interpreter then the one used to launch debugpy itself.
-                    -- The code below looks for a `venv` or `.venv` folder in the current directly and uses the python within.
-                    -- You could adapt this - to for example use the `VIRTUAL_ENV` environment variable.
-                    return os.getenv("CONDA_PREFIX") .. "\\Scripts\\python"
-                end,
-            },
-        }
-    end
+                    -- Options below are for debugpy, see https://github.com/microsoft/debugpy/wiki/Debug-configuration-settings for supported options
+                    program = "${file}", -- This configuration will launch the current file if used.
+                    -- pythonPath = "C:/Users/heihi/miniconda3/envs/torch20/python.exe"
+                    pythonPath = function()
+                        local is_windows = function()
+                            return vim.loop.os_uname().sysname:find("Windows", 1, true) and true
+                        end
+                        venv_path = os.getenv("CONDA_PREFIX")
+                        if venv_path then
+                            if is_windows() then
+                                return venv_path .. '\\python.exe'
+                            end
+                            return venv_path .. '/bin/python'
+                        end
+                    end
+                },
+            }
+        end
+    }
 }
